5b1d8b7fbbde51cef1267a20ba50cf5f4bd215b6
Nafees <abdur17@student.sust.edu>
2022-03-26 04:16:23 +0600

    fake data fetched

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Products = (props) => {
-    const {name, img1, price} = props.product;
+    const {name, img2, price} = props.product;
     
     return (
         <div className='ptoduct grid grid-cols-3'>
-            <img src={img1} alt="" />
+            <img src={img2} alt="" />
             <p>Name: {name}</p>
             <p>Price: {price}</p>
         </div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -5,7 +5,7 @@ import Products from '../../components/Products/Products';
 const Shop = () => {
     const [products, setProducts] = useState([]);
     useEffect(() => {
-        fetch('products.json')
+        fetch('https://run.mocky.io/v3/f0017a55-8f12-4b1b-88a7-850a64e46a3e')
             .then(res => res.json())
             .then(data => setProducts(data));
         console.log(products);
