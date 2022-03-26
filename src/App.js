efc1f018d2ca5016cded688763b9eba16200755d
Abdur Rahman <53124391+nafeescse@users.noreply.github.com>
2022-03-26 23:28:07 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-// import logo from './favicon.png';
 import './App.css';
 import Header from './components/Header/Header.js';
 import Shop from './components/Shop/Shop.js';
+import Answers from './components/Answers/Answers';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Shop></Shop>
+      <Answers></Answers>
     </div>
   );
 }
